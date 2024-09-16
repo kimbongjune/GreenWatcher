91a431d18e0fac08dbb49440a17fadd490c816cd
kimbongjune <nocdu112@naver.com>
2024-09-16 14:00:37 +0900

    Initial commit

---
+++ src/main/java/com/green/watcher/greenwatcher/GreenWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.green.watcher.greenwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/watcher/greenwatcher/GreenWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.watcher.greenwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
